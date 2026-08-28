2b6227431b114ba7ac753a72cf9f36397c1d5b59
Mukesh9116 <146856075+Mukesh9116@users.noreply.github.com>
2026-08-28 16:00:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotesai-x8ze.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
